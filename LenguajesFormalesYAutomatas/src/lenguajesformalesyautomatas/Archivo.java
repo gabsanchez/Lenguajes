f657ed912480822c6f41650d71b7c021d38376a0
gabsanchez <gabsanchez438@gmail.com>
2017-02-22 15:36:32 -0600

    Comp

--- LenguajesFormalesYAutomatas/src/lenguajesformalesyautomatas/Archivo.java
+++ LenguajesFormalesYAutomatas/src/lenguajesformalesyautomatas/Archivo.java
@@ -17,24 +17,37 @@ import java.io.RandomAccessFile;
 public class Archivo 
 {
     FileDialog fd = null;
-    RandomAccessFile lector = null;
+    RandomAccessFile lector1 = null;
+    RandomAccessFile lector2 = null;
     Principal form = new Principal();
     String nombreArchivo;
+    byte[] buffer1 = null;
+    byte[] buffer2 = null;
     
     public Archivo()
     {
         fd = new FileDialog(form, "Abrir archivo", FileDialog.LOAD);
-        fd.setDirectory("C:\\home\\gabriel");
     }
     public void Cargar() throws IOException
     {
         fd.setVisible(true);
         nombreArchivo = fd.getDirectory() + fd.getFile();
-        Leer(nombreArchivo);
+        Leer(nombreArchivo, 1);
     }
-    public void Leer(String nombreA) throws FileNotFoundException, IOException
+    public void Leer(String nombreA, int size) throws FileNotFoundException, IOException
     {
-        lector = new RandomAccessFile(nombreA, "r");
-        System.out.println(lector.length());
+        lector1 = new RandomAccessFile(nombreA, "r");
+        lector2 = new RandomAccessFile(nombreA, "r");
+        buffer1 = new byte[size];
+        buffer2 = new byte[size];
+        System.out.println(lector1.length());
+        lector1.seek(25);
+        lector2.seek(26);
+        lector1.read(buffer1);
+        lector2.read(buffer2);
+        lector1.close();
+        lector2.close();
+        System.out.println(new String(buffer1));
+        System.out.println(new String(buffer2));
     }
 }
