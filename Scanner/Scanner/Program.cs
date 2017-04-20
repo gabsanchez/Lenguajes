c3f7bee8379385eb09018d889bc3dd793a7565cf
Gabriel <gabsanchez438@gmail.com>
2017-04-20 16:54:06 -0600

    Add files via upload

---
+++ Scanner/Scanner/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace Scanner
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                //OpenFileDialog ofd = new OpenFileDialog();
+                Console.WriteLine("Ingrese la ruta del archivo de entrada");
+                string nombre = Console.ReadLine();
+                LectorEscritor macho = new LectorEscritor();
+                macho.CargarArchivo(nombre);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
